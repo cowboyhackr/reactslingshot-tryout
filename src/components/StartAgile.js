a3a7d060ce1e1827de32c0cff384467143045e14
mckenna <john.m.mckenna@live.com>
2016-08-30 07:52:26 -0500

    trying out react slingshot

---
+++ src/components/StartAgile.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const StartAgile = () => {
+  return (
+    <div>
+      <h1>Start Agile</h1>
+
+      <h2>Get Started</h2>
+      // <ol>
+      //   <li>Review the <Link to="fuel-savings">demo app</Link></li>
+      //   <li>Remove the demo and start coding: npm run remove-demo</li>
+      // </ol>
+    </div>
+  );
+};
+
+export default StartAgile;
